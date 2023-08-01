4d0506481a6bcfb12101cd05bcf288fbe5e6ebef
Matt Gawarecki <matt.gawarecki@automattic.com>
2023-08-01 08:13:57 -0500

    selectAlphabeticallySortedProductOptions: add test coverage

---
+++ client/jetpack-cloud/sections/partner-portal/lib/test/select-alphabetically-sorted-product-options.ts
@@ -0,0 +1,37 @@
+import { APIProductFamilyProduct } from '../../../../../state/partner-portal/types';
+import selectAlphabeticallySortedProductOptions from '../select-alphabetically-sorted-product-options';
+
+const createFakeProduct = ( name: string ) => ( {
+	name,
+	slug: '',
+	product_id: 0,
+	currency: '',
+	amount: 0,
+	price_interval: '',
+	family_slug: '',
+} );
+
+const createFakeFamily = ( products: APIProductFamilyProduct[] ) => ( {
+	name: '',
+	slug: '',
+	products,
+} );
+
+describe( 'selectAlphabeticallySortedProductOptions', () => {
+	it( 'returns all products from all families, sorted by their name', () => {
+		const apple = createFakeProduct( 'apple' );
+		const banana = createFakeProduct( 'banana' );
+		const kiwi = createFakeProduct( 'kiwi' );
+		const pear = createFakeProduct( 'pear' );
+
+		const result = selectAlphabeticallySortedProductOptions( [
+			createFakeFamily( [ banana, pear ] ),
+			createFakeFamily( [ apple, kiwi ] ),
+		] );
+
+		expect( result[ 0 ] ).toBe( apple );
+		expect( result[ 1 ] ).toBe( banana );
+		expect( result[ 2 ] ).toBe( kiwi );
+		expect( result[ 3 ] ).toBe( pear );
+	} );
+} );
